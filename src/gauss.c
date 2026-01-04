cd6ec3f4e384dd82255abcdb821cc1884b894047
DawidVoida <01206837@pw.edu.pl>
2026-01-04 15:52:50 +0100

    Update gauss.c

--- src/gauss.c
+++ src/gauss.c
@@ -5,10 +5,19 @@
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
-
-		return 0;
+	int n = mat->r;
+	for(int k = 0; k < n - 1; k++) {
+		if(mat->data[k][k] == 0) {
+			return 1;
+		}
+		for(int i = k + 1; i < n; i++) {
+			double factor = mat->data[i][k] / mat->data[k][k];
+			for(int j = k; j < n; j++) {
+				mat->data[i][j] -= factor * mat->data[k][j];
+			}
+			b->data[i][0] -= factor * b->data[k][0];
+		}
+	}
+	return 0;
 }
 
